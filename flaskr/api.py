ab0788d3ece64f9c454d9f0ba9cbf226e13130b0
PHTLing <145812848+PHTLing@users.noreply.github.com>
2024-05-13 02:48:18 +0700

    hehe

---
+++ flaskr/api.py
@@ -0,0 +1,36 @@
+import functools
+import json
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from flaskr.db import get_db
+
+bp = Blueprint('api', __name__, url_prefix='/api')
+
+
+@bp.route('/login', methods=["GET","POST"])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            "SELECT * FROM user WHERE username = ?", (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not user['password']== password: #check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return {"message":"login successful"}, 200
+
+        flash(error)
+
+    return {"message":"login failed"}, 401
